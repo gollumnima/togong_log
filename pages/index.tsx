2c5514093d178d99e042020911c0105e3492053e
Doori Kim <doori.alice.kim@gmail.com>
2022-07-17 23:34:30 +0900

    [Wip] 블로그 공사중

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,9 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
-import { getAllPosts } from '~/lib/posts';
 
-const Home: NextPage = ({ entries }) => (
+const Home: NextPage = () => (
   <section>
-    <h2>Recent Posts</h2>
-    <ul>
-      {entries.map((entry: string) => (
-        <li key={entry}>
-          <Link href="/posts/[post]" as={`/posts/${entry}`}>
-            <a>{entry}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <h2>🪓 토공은 공사중 🪓</h2>
   </section>
 );
 
 export default Home;
-
-export async function getStaticProps() {
-  const posts = await getAllPosts();
-
-  return {
-    props: {
-      entries: posts.map((post) => post.slug),
-    },
-  };
-}
